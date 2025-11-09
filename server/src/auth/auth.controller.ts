8868df78d0b80935d939c4a8783f85d89a15b365
Burak Kılıç <brkzenca@gmail.com>
2025-11-09 20:53:41 +0300

    NEX-005: feat(auth): Add JWT strategy and guard for protected routes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,16 +1,36 @@
 // server/src/auth/auth.controller.ts
 
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Get,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
-
-// AuthDto adında bir dosya oluşturup 'validation' (doğrulama) yapabilirdik
-// ama şimdilik 'any' kullanarak basit tutuyoruz.
+import { JwtAuthGuard } from './jwt-auth.guard';
+import type { Request } from 'express';
 
 @Controller('auth') // Bu controller'daki tüm endpoint'ler /auth ile başlar
 export class AuthController {
   constructor(private authService: AuthService) {}
-
+  /**
+   * GET /auth/profile
+   * Bu endpoint, SADECE geçerli bir JWT token'ı olan
+   * kullanıcılar tarafından erişilebilir.
+   */
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@Req() req: Request) {
+    // 'JwtStrategy'mizin 'validate' metodunda ne döndürdüysek
+    // (yani { userId: ..., email: ... }),
+    // NestJS onu 'req.user' içine koyar.
+    return req.user;
+  }
   /**
    * POST /auth/login
    */
