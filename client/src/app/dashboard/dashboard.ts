6892792965302a8caefa0d9e45d3d0d25a3fb252
Burak Kılıç <brkzenca@gmail.com>
2025-11-16 23:23:41 +0300

    NEX-014: feat(client): Refactor dashboard as main widget hub

--- client/src/app/dashboard/dashboard.ts
+++ client/src/app/dashboard/dashboard.ts
@@ -1,55 +1,21 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService, UserProfile } from '../auth.service';
-import { Router } from '@angular/router'; 
+import { Router, RouterLink } from '@angular/router'; 
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink], 
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard implements OnInit {
+
+export class Dashboard {
   // Servisleri "enjekte" et
   private authService = inject(AuthService);
   private router = inject(Router);
-  // Ekranda göstermek için değişkenler
-  userEmail: string | null = null;
-  apiError: string = '';
 
-  ngOnInit(): void {
-    // Auth API'mize istek at
-    this.authService.getProfile().subscribe({
-      
-      /**
-       * 'next' (Başarılı Durum):
-       * Interceptor'ımız (Adım 2) token'ı başarıyla ekledi
-       * ve NestJS bize kullanıcı profilini döndürdü.
-       */
-      next: (profile: UserProfile) => {
-        this.userEmail = profile.email;
-        this.apiError = ''; // Hata yok
-      },
-
-      /**
-       * 'error' (Hata Durumu):
-       * Interceptor çalışmadı VEYA token geçersiz/süresi dolmuş VEYA
-       * sunucu kapalı.
-       * NestJS bize 401 Unauthorized hatası döndürdü.
-       */
-      error: (err) => {
-        this.userEmail = null;
-        this.apiError = 'Profil verisi alınamadı. Lütfen tekrar giriş yapın.';
-        console.error('Profile Hatası:', err);
-        
-      },
-    });
-  }
-
-  /**
-   * Çıkış Yap (Logout) Fonksiyonu
-   */
   logout(): void {
     // 1. Token'ı localStorage'dan sil
     this.authService.logout();
@@ -57,4 +23,5 @@ export class Dashboard implements OnInit {
     // 2. Kullanıcıyı /login sayfasına geri yönlendir
     this.router.navigate(['/login']);
   }
-}
+} 
+
